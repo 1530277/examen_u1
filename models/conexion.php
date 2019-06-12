b68e4f6a03d75d56a64cbb20a06c0affef6571f9
Luissssssssssssssssssssssssss <1530277@upv.edu.mx>
2019-06-12 15:59:46 -0500

    passw

--- models/conexion.php
+++ models/conexion.php
@@ -1,7 +1,7 @@
 <?php
 	class Conexion{
 		public function load(){
-			$mbd = new PDO("mysql:host=localhost;dbname=examen_u1","root","");
+			$mbd = new PDO("mysql:host=localhost;dbname=examen_u1","1530277","Holamundo11");
 			$mbd -> exec("set names utf8");
 			return $mbd;
 		}
